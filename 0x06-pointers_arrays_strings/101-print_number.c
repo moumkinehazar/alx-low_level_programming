ddb82b51007fb311ad777752e7407ee8e79082e1
moumkinehhazar <moumkine.hazar@gmail.com>
2023-09-20 18:57:56 +0100

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,38 +1,25 @@
-alx-low_level_programming/0x06-pointers_arrays_strings/101-print_number.c
-@EmadAnwer
-EmadAnwer task 9
-Latest commit 391eb37 yesterday
- History
- 1 contributor
-30 lines (27 sloc)  385 Bytes
-
-/*
- * 0x06. C - More pointers, arrays and strings
- * task 9
- */
 #include "main.h"
+
 /**
- * print_number - encodes a string into  rot13.
- * @n: number
- * Return: *src encodd string into  rot13.
- */
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int x;
 
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
+		n *= -1;
 	}
 
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
+	x = n;
 
-	_putchar((n1 % 10) + '0');
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
