da782b8055f5334b9b78309673bbd6d832d11bf8
moumkinehhazar <moumkine.hazar@gmail.com>
2023-10-26 17:02:35 +0100

    push

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,17 +1,19 @@
-include "main.h"
-
+#include "main.h"
+#include <stdio.h>
 /**
-*get_bit - get the value of a bit at the given index
-*@n: the number
-*@index: index starting from 0 of the bit required
-*
-*Return: The converted value
-*/
-
+ * get_bit - get the value of a bit at a given index
+ * @n: number to evaluate
+ * @index: index starting from 0, of the bit we want to get
+ * Return: Value of bit at index, or -1 if error
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 63)
+	unsigned long int hold;
+
+	if (index > 64)
 		return (-1);
 
-	return ((n >> index) & 1);
+	hold = n >> index;
+
+	return (hold & 1);
 }
